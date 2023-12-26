324a73f07aa72efb8691e636efa61dd33fda9364
anas <aneddameanas2@gmail.com>
2023-12-26 10:37:14 +0100

    Project Setup

---
+++ src/main/java/com/example/sri_thematique_voyage/SriThematiqueVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.example.sri_thematique_voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriThematiqueVoyageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SriThematiqueVoyageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sri_thematique_voyage/SriThematiqueVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sri_thematique_voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriThematiqueVoyageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
