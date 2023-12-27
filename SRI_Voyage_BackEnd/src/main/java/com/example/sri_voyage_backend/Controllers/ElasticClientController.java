3a39f39d29c441ca6e6e1015dd60f4398e51fb03
nizarBouhsaine <you@example.com>
2023-12-27 10:24:31 +0100

    Test Elasticsearch query

--- SRI_Voyage_BackEnd/src/main/java/com/example/sri_voyage_backend/Controllers/ElasticClientController.java
+++ SRI_Voyage_BackEnd/src/main/java/com/example/sri_voyage_backend/Controllers/ElasticClientController.java
@@ -3,6 +3,9 @@ package com.example.sri_voyage_backend.Controllers;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.analysis.*;
+import co.elastic.clients.elasticsearch._types.mapping.Property;
+import co.elastic.clients.elasticsearch._types.query_dsl.Operator;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.indices.*;
@@ -30,7 +33,6 @@ public class ElasticClientController {
     String serverUrl = "https://48cf8c7b250b4bd1984f99117884d19c.us-central1.gcp.cloud.es.io";
     String apiKey = "T2J1bHBZd0JXOUNQRVllZWp1c3g6QXpadjhZMVpTVHllektROGpiUFBJZw==";
 
-    // Create the low-level client
     RestClient restClient = RestClient
             .builder(HttpHost.create(serverUrl))
             .setDefaultHeaders(new Header[]{
@@ -38,33 +40,31 @@ public class ElasticClientController {
             })
             .build();
 
-    // Create the transport with a Jackson mapper
     ElasticsearchTransport transport = new RestClientTransport(
             restClient, new JacksonJsonpMapper());
-
-    // And create the API client
     ElasticsearchClient esClient = new ElasticsearchClient(transport);
-//        esClient.indices().create(c -> c
-//                .index("products")
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 //
-//        );
-//        Product product = new Product("bk-2", "City bike", 123.0);
+//                Product product = new Product("ph-1", "City Phone", 123.0,"Best Phone in the world !");
 //
-//        IndexResponse response = esClient.index(i -> i
-//                .index("products")
-//                .id(product.getSku())
-//                .document(product)
-//        );
+        Map<String, Property> propertyMap = new HashMap<>();
+        propertyMap.put("id", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("name", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("city", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("duration", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("price", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("link", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("description", new Property.Builder().text(t->t.analyzer("french")).build());
+        propertyMap.put("type", new Property.Builder().text(t->t.analyzer("french")).build());
 
-//        CreateIndexRequest.Builder createIndexRequest = new CreateIndexRequest.Builder();
-//        IndexSettings.Builder indexsettings = new IndexSettings.Builder();
-        IndexSettingsAnalysis.Builder indexSettingsAnalysis = new IndexSettingsAnalysis.Builder();
-        TokenFilter.Builder frenchTokenb = new TokenFilter.Builder();
 
-        TokenFilterDefinition.Builder tokenfilterb = new TokenFilterDefinition.Builder();
-        ElisionTokenFilter.Builder elision = new ElisionTokenFilter.Builder();
-
-        elision.articles(List.of("a", "afin", "ai", "ainsi", "après", "attendu", "au", "aujourd", "auquel", "aussi",
+        List<String> stopWords = List.of("a", "afin", "ai", "ainsi", "après", "attendu", "au", "aujourd", "auquel", "aussi",
                 "autre", "autres", "aux", "auxquelles", "auxquels", "avait", "avant", "avec", "avoir",
                 "c", "car", "ce", "ceci", "cela", "celle", "celles", "celui", "cependant", "certain",
                 "certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "chez", "ci",
@@ -85,68 +85,112 @@ public class ElasticClientController {
                 "son", "sont", "sous", "suivant", "sur", "ta", "te", "tes", "tien", "tienne", "tiennes",
                 "tiens", "toi", "ton", "tous", "tout", "toute", "toutes", "tu", "un", "une", "va", "vers",
                 "voici", "voilà", "vos", "votre", "vous", "vu", "vôtre", "vôtres", "y", "à", "ça", "ès",
-                "été", "être", "ô"));
-        TokenFilterDefinition tokenFilterdef = tokenfilterb.elision(elision.build()).build();
-//        StopTokenFilter.Builder stopToken = new StopTokenFilter.Builder();
-//        stopToken.stopwords("au","d'");
-//        tokenfilterb.stop(stopToken.build());
-
-        TokenFilter frenchToken = frenchTokenb.definition(tokenFilterdef).build();
-
-
-
-
-        System.out.println("Get : "+ frenchToken._get().toString());
-        System.out.println("Kind : "+ frenchToken._kind().toString());
-
-//        Analyzer.Builder analyzer = new Analyzer.Builder();
-//        CustomAnalyzer.Builder customanalyzer = new CustomAnalyzer.Builder();
+                "été", "être", "ô");
 //
-        Tokenizer.Builder tokenBuilder = new Tokenizer.Builder();
-        Tokenizer token =  tokenBuilder.name("standard").build();
+//        CreateIndexResponse responseAnalyser = esClient.indices().create(
+//                i-> i.index("voyage")
+//                        .settings(
+//                                s->s.analysis(
+//                                        a->a.filter("french_stop",sd->sd.definition(ff->ff.stop(sw->sw.stopwords(stopWords)))).analyzer("rebuilt_french",
+//                                                f->f.custom(v->v.tokenizer("standard").filter("lowercase","french_stop")))
+//                                )
+//                        )
+//                        .mappings(
+//                        m->m.properties(propertyMap)
+//                        ));
+//        Product product = new Product("bk-1", "Vélo de montagne", 123.0,"Le vélo de montagne enduro électrique SANTA CRUZ Heckler 9 2022 n'a pas pour but de rendre les choses faciles, mais de les rendre possibles. Il est muni d'un moteur Shimano et d'une batterie de 720 Wh pour une puissance accrue.");
+//        Product product = new Product("Mo-1", "Souris Gamer", 123.0,"Travaillez librement avec la souris Logitech M190. Sans fil, elle vous permet de vous déplacer librement jusqu'à 10 mètres sans délai ni interférences. De plus, sa forme, de taille standard, est adaptée à la main droite comme à la main gauche et permet de positionner sa main confortablement. Et avec un capteur optique de 1000 dpi de la souris M190 vous garantit un contrôle réactif et fluide du pointeur, permettant un suivi précis et une sélection de texte aisée.");
 
-//        System.out.println(token._get());
-        List<String> texts = new ArrayList<>();
-        String searchText = "";
-        texts.add(searchText);
 
-        TokenFilter.Builder tokenFilter = new TokenFilter.Builder();
-        TokenFilter tokenFilter1 = tokenFilter.name("stop").build();
-//        TokenFilter.Builder tokenFilterbuilder = new TokenFilter.Builder();
+//        SearchDocument searchDocument = new SearchDocument(
+//                "1",
+//                "Un séjour actif durant vos vacances La montagne pour terrain de jeu",
+//                "",
+//                "",
+//                "",
+//                "",
+//                ""
+//        );
 
-//        TokenFilter tokenFilter2 = tokenFilterbuilder.name("keyword_repeat").build();
+
+
+
+//        IndexResponse responseadd = esClient.index(i -> i
+//                .index("french_analyzer")
+//                .id(product.getSku())
+//                .document(product)
+//        );
+
+//        AnalyzeRequest analyzeRequest = analyzeRequestBuilder.index("french_analyzer").text("je veux savoir s'il est possible de voir les cacade d'ouzode à partir d'un bateau").build();
+//        AnalyzeResponse analyzeResponse = esClient.indices().analyze(
+//                analyzeRequest
+//        );
         AnalyzeRequest.Builder analyzeRequestBuilder = new AnalyzeRequest.Builder();
+
+        TokenFilter.Builder tokenfb = new TokenFilter.Builder();
+        AnalyzeRequest analyzeRequest = analyzeRequestBuilder.analyzer("french").text("je cherche un bon vélo pour traverser les montagnes du maroc").build();
         AnalyzeResponse analyzeResponse = esClient.indices().analyze(
-                analyzeRequestBuilder.tokenizer(token).filter(frenchToken).text(texts).build()
+                analyzeRequest
         );
-        String quote = "journée au belle cascade d'ouzoud";
-        for(AnalyzeToken tok: analyzeResponse.tokens())
+
+        String quote ="";
+        for(AnalyzeToken tok : analyzeResponse.tokens())
         {
             quote=quote+" "+tok.token();
         }
-        System.out.println(quote);
+        ///////////////////////////////////////////////////////////////////////////////////
 
-
+//        String quote = "journée au belle cascade d'ouzoud";
+//        for(AnalyzeToken tok: analyzeResponse.tokens())
+//        {
+//            quote=quote+" "+tok.token();
+//        }
+//        System.out.println(quote);
+//
+//
         String finalQuote = quote;
-        SearchResponse<SearchDocument> response = esClient.search(s -> s
-                        .index("voyages")
+        System.out.println(finalQuote);
+//        SearchResponse<SearchDocument> response = esClient.search(s -> s
+//                        .index("voyages")
+//                        .query(q -> q
+//                                .match(t -> t
+//                                        .field("description")
+//                                        .query(finalQuote)
+//                                )
+//                        ),
+//                SearchDocument.class
+//        );
+        SearchResponse<Product> response = esClient.search(s -> s
+                        .index("french_analyzer")
                         .query(q -> q
                                 .match(t -> t
                                         .field("description")
                                         .query(finalQuote)
-
                                 )
                         ),
-                SearchDocument.class
+                Product.class
         );
-
+//
+//
+//        for(Hit h:response.hits().hits())
+//        {
+//            SearchDocument s = (SearchDocument) h.source();
+//            System.out.println("/////////////////////////////////////////////");
+//            System.out.println("City");
+//            System.out.println(s.getCity());
+//            System.out.println("Description");
+//            System.out.println(s.getDescription());
+//            System.out.println("Score");
+//            System.out.println(h.score());
+//
+//        }
 
         for(Hit h:response.hits().hits())
         {
-            SearchDocument s = (SearchDocument) h.source();
+            Product s = (Product) h.source();
             System.out.println("/////////////////////////////////////////////");
-            System.out.println("City");
-            System.out.println(s.getCity());
+            System.out.println("name");
+            System.out.println(s.getName());
             System.out.println("Description");
             System.out.println(s.getDescription());
             System.out.println("Score");
