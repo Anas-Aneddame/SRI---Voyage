bff8853bacd9114b978c9f4630a9278b7bb201fd
hamza-elgarai <hamzaelgarai10@gmail.com>
2023-12-27 20:23:29 +0100

    fixed minor bug

--- SRI_Voyage_FrontEnd/src/app/search/search.component.ts
+++ SRI_Voyage_FrontEnd/src/app/search/search.component.ts
@@ -147,6 +147,7 @@ export class SearchComponent implements AfterViewInit{
     this.zone.run(() => {
       this.searchQuery = "";
       this.isExpanded = false;
+      this.areResultsRendered = false;
     });
   }
 
