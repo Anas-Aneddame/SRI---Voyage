3e77986555a4d08922b6db6fb32f556d659cf18f
anas <aneddameanas2@gmail.com>
2023-12-27 11:48:53 +0100

    link frontEnd and backEnd

--- SRI_Voyage_FrontEnd/src/app/search/search.component.ts
+++ SRI_Voyage_FrontEnd/src/app/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { PopupService } from '../popup/popup.service';
 import { MatDialog } from '@angular/material/dialog';
 import { FilterComponent } from '../filter/filter.component';
+import { HttpClient } from '@angular/common/http';
+
 
 
 
@@ -15,7 +16,8 @@ export class SearchComponent {
   searchQuery: string = '';
   filterData: any;
 
-  constructor(public dialog: MatDialog,) {}
+  constructor(public dialog: MatDialog,private httpClient: HttpClient) {}
+
 
 
 
@@ -31,7 +33,20 @@ export class SearchComponent {
         console.log('Dialog closed with result:', result);
       });
     }
-  search() {
+  /*search() {
     console.log('Searching for:', this.searchQuery);
+
+  }
+  */
+  
+
+  search() {
+    // Effectuez la requête HTTP vers votre backend Spring Boot
+    const apiUrl = `http://localhost:8090/query/${this.searchQuery}`;
+
+    this.httpClient.get(apiUrl).subscribe((response) => {
+      // Traitez la réponse ici, par exemple, affichez-la dans la console
+      console.log('Search results:', response);
+    });
   }
 }
